254c777341ace37d23ffbc25cd92f54d3eb21650
ambleandro <ambleandro@gmail.com>
2024-09-18 19:03:23 -0300

    Add Exercice

---
+++ 01_Exercice/scripts.js
@@ -0,0 +1,57 @@
+const form = document.querySelector("form");
+const nome = document.querySelector("#nome");
+const email = document.querySelector("#email");
+const assunto = document.querySelector("#assunto");
+const mensagem = document.querySelector("#mensagem");
+const errorMessages = document.querySelectorAll(".error-message");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  resetErrors()
+  validateInputs()
+})
+
+function setError(input, errorMessage) {
+  const errorMessageElement = input.nextElementSibling;
+  errorMessageElement.innerHTML = errorMessage;
+  input.parentElement.classList.add("error");
+}
+
+function resetErrors() {
+  errorMessages.forEach((msg) => {
+    msg.innerHTML = "";
+  })
+  nome.parentElement.classList.remove("error");
+  email.parentElement.classList.remove("error");
+  assunto.parentElement.classList.remove("error");
+  mensagem.parentElement.classList.remove("error");
+}
+
+function validateInputs() {
+  const nomeValue = nome.value.trim();
+  const emailValue = email.value.trim();
+  const assuntoValue = assunto.value.trim();
+  const mensagemValue = mensagem.value.trim();
+
+  if (nomeValue === "") {
+    setError(nome, `<i class="bi bi-exclamation-circle-fill"></i> Nome não pode ficar em branco.`)
+  }
+
+  if (emailValue === "") {
+    setError(email, `<i class="bi bi-exclamation-circle-fill"></i> E-mail não pode ficar em branco.`)
+  } else if(!isValidEmail(emailValue)) {
+    setError(email, "E-mail inválido")
+  }
+
+  if (assuntoValue === "") {
+    setError(assunto, `<i class="bi bi-exclamation-circle-fill"></i> Assunto não pode ficar em branco.`)
+  }
+
+  if (mensagemValue === "") {
+  setError(mensagem, `<i class="bi bi-exclamation-circle-fill"></i> Mensagem não pode ficar em branco.`)
+  }
+}
+
+function isValidEmail(email) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
